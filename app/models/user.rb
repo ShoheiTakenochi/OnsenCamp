3c0a4ea1bfec9d62bcefb79bdfd78a99ec261411
ShoheiTakenochi <takeno1221@gmail.com>
2025-03-11 21:54:17 +0900

    ’ログイン/ログアウト機能の実装’

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username, length: { maximum: 20 }
+
 end
