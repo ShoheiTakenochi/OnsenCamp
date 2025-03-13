4019112dc8333763ef3cfcedd191cb54dbfa3ef2
ShoheiTakenochi <takeno1221@gmail.com>
2025-03-14 01:23:11 +0900

    ’キャンプ場検索バーの作成’

--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -2,6 +2,7 @@ class CampsitesController < ApplicationController
   def show; end
 
   def index
-
+    @q = Campsite.ransack(params[:q])
+    @campsites = @q.result(distinct: true)
   end
 end
--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,2 +1,5 @@
 class Campsite < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["address", "name"]
+  end
 end
