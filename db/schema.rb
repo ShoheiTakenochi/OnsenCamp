6e699774f36025882b5611e830f6ec8986552fa6
ShoheiTakenochi <takeno1221@gmail.com>
2025-03-18 23:06:29 +0900

    ’キャンプ場詳細ページの作成’

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_17_111021) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_18_121139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_17_111021) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "photo_references"
+    t.jsonb "photo_references"
   end
 
   create_table "users", force: :cascade do |t|
