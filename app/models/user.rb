9ee7e265eb71f2477e0d56c61e5c15f74b6c1d49
ShoheiTakenochi <takeno1221@gmail.com>
2025-04-03 23:54:03 +0900

    ’不具合を修正’

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, allow_nil: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, length: { maximum: 20 }
 
   def favorite?(campsite)
