99944402b220016ded81479637efbd77097069fe
ShoheiTakenochi <takeno1221@gmail.com>
2025-04-12 21:15:33 +0900

    ’CIの導入’

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,7 +10,10 @@ Rails.application.configure do
 
   # While tests run files are not watched, reloading is not necessary.
   config.enable_reloading = false
-
+  
+  config.assets.css_compressor = nil # tailwindとsprocketsの相性回避
+  config.assets.compile = true       # コンパイル許可
+  config.assets.debug = true         # デバッグ用。必要ならfalseに
   # Eager loading loads your entire application. When running a single test locally,
   # this is usually not necessary, and can slow down your test suite. However, it's
   # recommended that you enable it in continuous integration systems to ensure eager
