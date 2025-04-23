a68aa747ebf10412df73f2ba852fbbbda75e1176
ShoheiTakenochi <takeno1221@gmail.com>
2025-04-23 23:27:42 +0900

    ’snsログイン機能の実装’

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_31_052910) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_22_093125) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,6 +85,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_31_052910) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "profile_image"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
