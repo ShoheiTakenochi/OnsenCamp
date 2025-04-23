b9ea40c59135c7c9f24e47e12d0d6c7eb983c29e
ShoheiTakenochi <takeno1221@gmail.com>
2025-04-23 23:48:22 +0900

    ’ファビコンの設定’

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,6 +10,27 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+    if resource.provider.present?
+      # 🔥 current_password を除いたパラメータだけで更新
+      result = resource.update_without_password(account_update_params.except(:current_password))
+    else
+      result = update_resource(resource, account_update_params)
+    end
+
+    if result
+      set_flash_message :notice, :updated if is_flashing_format?
+      bypass_sign_in resource, scope: resource_name
+      redirect_to after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
