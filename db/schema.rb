edd3ad287bd514a68602b1f5d80d678b5313478f
ShoheiTakenochi <takeno1221@gmail.com>
2026-02-25 20:35:47 +0900

    ’フレキシブルデザインに修正’

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_22_093125) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_31_052910) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,7 +87,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_22_093125) do
     t.string "profile_image"
     t.string "provider"
     t.string "uid"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
